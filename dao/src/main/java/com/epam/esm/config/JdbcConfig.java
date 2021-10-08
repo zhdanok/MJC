749d3c62a6b3552feca8f9540008dc671e9d60c4
zhdanok <zhdanokla@gmail.com>
2021-10-08 19:08:17 +0300

    removed comments

--- dao/src/main/java/com/epam/esm/config/JdbcConfig.java
+++ dao/src/main/java/com/epam/esm/config/JdbcConfig.java
@@ -11,7 +11,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 @ComponentScan(basePackages = "com.epam.esm")
-@PropertySource( "classpath:jdbc.properties")
+@PropertySource("classpath:jdbc.properties")
 @RequiredArgsConstructor
 public class JdbcConfig {
 
@@ -19,21 +19,12 @@ public class JdbcConfig {
     private final Environment environment;
 
     @Bean
-    public JdbcTemplate getJdbcTemplate()  {
-        /*BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
-        dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
-        dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
-        dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
-        dataSource.setMinIdle(5);
-        dataSource.setMaxIdle(10);
-        dataSource.setMaxOpenPreparedStatements(100);*/
-
+    public JdbcTemplate getJdbcTemplate() {
         return new JdbcTemplate(getDataSource());
     }
 
     @Bean
-    public BasicDataSource getDataSource(){
+    public BasicDataSource getDataSource() {
         BasicDataSource dataSource = new BasicDataSource();
         dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
         dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
