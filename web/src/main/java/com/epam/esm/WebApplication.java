b3a9f45bda4bee5bf50a42e928054c8629e83d30
zhdanok <zhdanokla@gmail.com>
2021-12-22 09:34:27 +0300

    added tomcat

--- web/src/main/java/com/epam/esm/WebApplication.java
+++ web/src/main/java/com/epam/esm/WebApplication.java
@@ -2,14 +2,20 @@ package com.epam.esm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebApplication {
+public class WebApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(WebApplication.class);
+    }
 }
